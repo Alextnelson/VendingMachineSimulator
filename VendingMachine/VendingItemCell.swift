99bcee20af9b5f1a2b5bdd77cfb17b1a3ce8d653
Alextnelson <alexander.thomas.nelson@gmail.com>
2016-03-05 19:15:22 -0600

    Added some code to account for plist

---
+++ VendingMachine/VendingItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  VendingMachine.swift
+//  VendingMachineSimulator
+//
+//  Created by Alexander Nelson on 3/4/16.
+//  Copyright © 2016 Jetwolfe Labs. All rights reserved.
+//
+
+import UIKit
+
+class VendingItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconView: UIImageView!
+    
+}
---
+++ VendingMachine/VendingMachine.swift
@@ -0,0 +1,123 @@
+//
+//  VendingMachine.swift
+//  VendingMachineSimulator
+//
+//  Created by Alexander Nelson on 3/4/16.
+//  Copyright © 2016 Jetwolfe Labs. All rights reserved.
+//
+
+import Foundation
+
+//Protocols
+
+protocol VendingMachineType {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: ItemType] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: ItemType])
+    
+    func vend(selection: VendingSelection, quantity: Double) throws
+    func deposit(amount: Double)
+}
+
+protocol ItemType {
+    var price: Double { get }
+    var quantity: Double { get set }
+}
+
+// Error Types
+
+enum InventoryError: ErrorType {
+    case InvalidResource
+    case ConversionError
+    case InvalidKey
+}
+
+// Helper Classes
+
+class PlistConverter {
+    class func dictionaryFromFile(resource: String, ofType type: String) throws ->  [String : AnyObject] {
+        //Code
+        guard let path = NSBundle.mainBundle().pathForResource(resource, ofType: type) else {
+            throw InventoryError.InvalidResource
+        }
+        
+        guard let dictionary = NSDictionary(contentsOfFile: path), let castDictionary = dictionary as? [String : AnyObject] else {
+            throw InventoryError.ConversionError
+        }
+        return castDictionary
+    }
+}
+
+class InventoryUnarchiver {
+    class func vendingInventoryFromDictionary(dictionary: [String : AnyObject]) throws -> [VendingSelection : ItemType] {
+        
+        var inventory: [VendingSelection : ItemType] = [ : ]
+        
+        for (key, value) in dictionary {
+            if let itemDict = value as? [String : Double], let price = itemDict["price"], let quantity = itemDict["quantity"] {
+                
+                let item = VendingItem(price: price, quantity: quantity)
+                
+                guard let key = VendingSelection(rawValue: key) else {
+                    throw InventoryError.InvalidKey
+                }
+                
+                inventory.updateValue(item, forKey: key)
+            }
+        }
+        
+        return inventory
+    }
+}
+
+// Concrete Types
+
+enum VendingSelection: String {
+    case Soda
+    case DietSoda
+    case Chips
+    case Cookie
+    case Sandwich
+    case Wrap
+    case CandyBar
+    case PopTart
+    case Water
+    case FruitJuice
+    case SportsDrink
+    case Gum
+}
+
+struct VendingItem: ItemType {
+    let price: Double
+    var quantity: Double
+}
+
+
+
+class VendingMachine: VendingMachineType {
+    let selection: [VendingSelection] = [.Soda, .DietSoda, .Chips, .Cookie, .Sandwich, .Wrap, .CandyBar, .PopTart, .Water, .FruitJuice, .SportsDrink, .Gum]
+    var inventory: [VendingSelection: ItemType]
+    var amountDeposited: Double = 10.0
+    
+    required init(inventory: [VendingSelection : ItemType]) {
+        self.inventory = inventory
+    }
+    
+    func vend(selection: VendingSelection, quantity: Double) throws {
+        //add code
+    }
+    
+    func deposit(amount: Double) {
+        //add code
+    }
+}
+
+
+
+
+
+
+
+
